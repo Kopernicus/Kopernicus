041ca0dc8baed18cd267f5d1a9b19cd80be74207
ThomasKerman <dorianstoll@googlemail.com>
2015-03-23 17:23:02 +0100

    Implemented star alternator

--- Kopernicus/StarLightSwitcher.cs
+++ Kopernicus/StarLightSwitcher.cs
@@ -34,7 +34,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-
 using UnityEngine;
 
 namespace Kopernicus
@@ -48,6 +47,15 @@ namespace Kopernicus
         // Custom powerCurves
         private Dictionary<string, ConfigNode> powerNodes = new Dictionary<string,ConfigNode>();
 
+        // StarTransforms
+        private Dictionary<string, Transform> starTransform = new Dictionary<string,Transform>();
+
+        // Store StarColors 
+        private Dictionary<string, Dictionary<string, Vector4>> starColor = new Dictionary<string, Dictionary<string, Vector4>>();
+
+        // StarTexture
+        private Dictionary<string, Texture2D> mainTex = new Dictionary<string,Texture2D>();
+
 		// MonoBehavior.Awake()
 		void Awake()
 		{
@@ -55,14 +63,52 @@ namespace Kopernicus
 			Debug.Log ("[Kopernicus]: StarLightSwitcher Started");
 			DontDestroyOnLoad (this);
 			stars = PSystemManager.Instance.localBodies.Where (body => body.scaledBody.GetComponentsInChildren<SunShaderController> (true).Length > 0).ToList ();
-            foreach (ConfigNode root in GameDatabase.Instance.GetConfigNodes("Kopernicus")) // I have NO idea, why this doesn't works....
-            {                                                                               // foreach (ConfigNode node in GameDatabase.Instance.GetConfigNode("Kopernicus").GetNodes("Body"))
-                foreach (ConfigNode node in root.GetNodes("Body"))
+
+            // Load custom powerCurves
+            foreach (ConfigNode node in GameDatabase.Instance.GetConfigNodes("Kopernicus")[0].GetNodes("Body"))
+            {                                                                               
+                if (node.HasNode("powerCurve"))
+                    powerNodes.Add(node.GetValue("name"), node.GetNode("powerCurve")); // Add custom powerCurve to our Dictionary
+                else
+                    powerNodes.Add(node.GetValue("name"), powerNodes["Sun"]);
+            }
+
+            // Get StarTransforms
+            foreach (Transform TransformStar in ScaledSpace.Instance.scaledSpaceTransforms)
+            {
+                starTransform[TransformStar.name] = TransformStar;
+            }
+
+            // Load StarColors
+            ConfigNode root = GameDatabase.Instance.GetConfigs("Kopernicus")[0].config;
+            foreach (ConfigNode body in root.GetNodes("Body"))
+            {
+                if (body.HasNode("StarColor"))
                 {
-                    if (node.HasNode("powerCurve"))
-                        powerNodes.Add(node.GetValue("name"), node.GetNode("powerCurve")); // Add custom powerCurve to our Dictionary
-                    else
-                        powerNodes.Add(node.GetValue("name"), powerNodes["Sun"]);
+                    ConfigNode color = body.GetNode("StarColor");
+                    Dictionary<string, Vector4> colorValues = new Dictionary<string, Vector4>();
+                    string bodyName = body.GetValue("name");
+                    colorValues.Add("lightColor", ConfigNode.ParseVector4(color.GetValue("lightColor") ?? "0,0,0,0"));
+                    colorValues.Add("emitColor0", ConfigNode.ParseVector4(color.GetValue("emitColor0") ?? "0,0,0,0"));
+                    colorValues.Add("emitColor1", ConfigNode.ParseVector4(color.GetValue("emitColor1") ?? "0,0,0,0"));
+                    colorValues.Add("sunspotColor", ConfigNode.ParseVector4(color.GetValue("sunspotColor") ?? "0,0,0,0"));
+                    colorValues.Add("rimColor", ConfigNode.ParseVector4(color.GetValue("rimColor") ?? "0,0,0,0"));
+
+                    mainTex.Add(bodyName, GameDatabase.Instance.GetTexture(color.GetValue("coronaTexture") ?? "", false));
+                    starColor.Add(bodyName, colorValues);
+                }
+            }
+
+            // Apply StarColor to Star
+            foreach (CelestialBody star in stars)
+            {
+                if (starColor.Keys.Contains(star.bodyName))
+                {
+                    starTransform[star.bodyName].renderer.material.SetColor("_EmitColor0", ColorConvert(starColor[star.bodyName]["emitColor0"]));
+                    starTransform[star.bodyName].renderer.material.SetColor("_EmitColor1", ColorConvert(starColor[star.bodyName]["emitColor1"]));
+                    starTransform[star.bodyName].renderer.material.SetColor("_SunspotColor", ColorConvert(starColor[star.bodyName]["sunspotColor"]));
+                    starTransform[star.bodyName].renderer.material.SetColor("_RimColor", ColorConvert(starColor[star.bodyName]["rimColor"]));
+                    
                 }
             }
 		}
@@ -97,8 +143,7 @@ namespace Kopernicus
             Debug.Log("Active sun set to: " + CB.name);
 
             //Set sunflare color
-			//TODO - Fix getting the sun flare color
-			//Sun.Instance.sunFlare.color = CB.light.color;
+            Sun.Instance.sunFlare.color = (starColor.Keys.Contains(CB.bodyName)) ? ColorConvert(starColor[CB.bodyName]["lightColor"]) : new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
             //Reset solar panels (Credit to Kcreator)
             foreach (ModuleDeployableSolarPanel panel in FindObjectsOfType(typeof(ModuleDeployableSolarPanel)))
@@ -106,6 +151,7 @@ namespace Kopernicus
                 panel.OnStart(PartModule.StartState.Orbital);
             }
 
+            // Set custom powerCurve for solar panels
             if (FlightGlobals.ActiveVessel != null)
             {
                 foreach (ModuleDeployableSolarPanel sp in FlightGlobals.ActiveVessel.FindPartModulesImplementing<ModuleDeployableSolarPanel>())
@@ -117,6 +163,21 @@ namespace Kopernicus
                     }
                 }
             }
+
+            // Update Corona texture
+            if (starTransform.Keys.Contains(CB.bodyName))
+            {
+                foreach (SunCoronas Corona in starTransform[CB.bodyName].GetComponentsInChildren<SunCoronas>())
+                {
+                    if (mainTex.Keys.Contains(CB.bodyName))
+                        Corona.renderer.material.mainTexture = mainTex[CB.bodyName];
+                }
+            }
         }
+
+        public Color ColorConvert(Vector4 c) {
+            return new Color(c.x, c.y, c.z, c.w);
+        }
+
     }
 }
