eda538c0229b00d8d54b0526b36a8160de0eaf04
ThomasKerman <dorianstoll@googlemail.com>
2015-03-31 17:36:17 +0200

    More Mods
    VertexHeightMapStep
    VertexHeightNoiseHeightMap
    VertexHeightNoiseVertHeight
    VertexHeightNoiseVertHeightCurve
    VertexHeightOblate
    
    Some of the mods are really silly....

---
+++ Kopernicus/Configuration/ModLoader/VertexHeightMapStep.cs
@@ -0,0 +1,102 @@
+﻿/**
+ * Kopernicus Planetary System Modifier
+ * Copyright (C) 2014 Bryce C Schroeder (bryce.schroeder@gmail.com), Nathaniel R. Lewis (linux.robotdude@gmail.com)
+ * 
+ * http://www.ferazelhosting.net/~bryce/contact.html
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * This library is intended to be used as a plugin for Kerbal Space Program
+ * which is copyright 2011-2014 Squad. Your usage of Kerbal Space Program
+ * itself is governed by the terms of its EULA, not the license above.
+ * 
+ * https://kerbalspaceprogram.com
+ */
+
+using System;
+using UnityEngine;
+
+namespace Kopernicus
+{
+	namespace Configuration
+	{
+		namespace ModLoader
+		{
+			[RequireConfigType(ConfigType.Node)]
+			public class VertexHeightMapStep : ModLoader, IParserEventSubscriber
+			{
+				// Actual PQS mod we are loading
+				private PQSMod_VertexHeightMapStep _mod;
+
+				// The map texture for the planet
+				[ParserTarget("map", optional = false)]
+				private MapSOParser_GreyScale<MapSO> heightMap
+				{
+					set { _mod.heightMap = value.value.CompileToTexture(); }
+				}
+
+				// Height map offset
+				[ParserTarget("offset", optional = true)]
+				private NumericParser<double> heightMapOffset 
+				{
+					set { _mod.heightMapOffset = value.value; }
+				}
+
+				// Height map offset
+				[ParserTarget("deformity", optional = true)]
+				private NumericParser<double> heightMapDeformity
+				{
+					set { _mod.heightMapDeformity = value.value; }
+				}
+
+				// Height map offset
+				[ParserTarget("scaleDeformityByRadius", optional = true)]
+				private NumericParser<bool> scaleDeformityByRadius
+				{
+					set { _mod.scaleDeformityByRadius = value.value; }
+				}
+
+				[ParserTarget("coastHeight", optional = true)]
+				private NumericParser<double> coastHeight
+				{
+					set { _mod.coastHeight = value.value; }
+                }
+
+
+				void IParserEventSubscriber.Apply(ConfigNode node)
+				{
+
+				}
+
+				void IParserEventSubscriber.PostApply(ConfigNode node)
+				{
+
+				}
+
+                public VertexHeightMapStep()
+				{
+					// Create the base mod
+					GameObject modObject = new GameObject ("VertexHeightMapStep");
+					modObject.transform.parent = Utility.Deactivator;
+					_mod = modObject.AddComponent<PQSMod_VertexHeightMapStep> ();
+					_mod.requirements = PQS.ModiferRequirements.MeshCustomNormals | PQS.ModiferRequirements.VertexMapCoords;
+					base.mod = _mod;
+				}
+			}
+		}
+	}
+}
+
---
+++ Kopernicus/Configuration/ModLoader/VertexHeightNoiseVertHeightCurve.cs
@@ -0,0 +1,143 @@
+﻿/**
+ * Kopernicus Planetary System Modifier
+ * Copyright (C) 2014 Bryce C Schroeder (bryce.schroeder@gmail.com), Nathaniel R. Lewis (linux.robotdude@gmail.com)
+ * 
+ * http://www.ferazelhosting.net/~bryce/contact.html
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * This library is intended to be used as a plugin for Kerbal Space Program
+ * which is copyright 2011-2014 Squad. Your usage of Kerbal Space Program
+ * itself is governed by the terms of its EULA, not the license above.
+ * 
+ * https://kerbalspaceprogram.com
+ */
+
+using System;
+using UnityEngine;
+
+namespace Kopernicus
+{
+	namespace Configuration
+	{
+		namespace ModLoader
+		{
+			[RequireConfigType(ConfigType.Node)]
+			public class VertexHeightNoiseVertHeightCurve : ModLoader, IParserEventSubscriber
+			{
+				// Actual PQS mod we are loading
+				private PQSMod_VertexHeightNoiseVertHeightCurve _mod;
+
+                // curve
+				[ParserTarget("curve", optional = true)]
+				private AnimationCurveParser curve
+				{
+					set { _mod.curve = value.curve; }
+				}
+                
+                // Where the height starts
+				[ParserTarget("heightStart", optional = true)]
+				private NumericParser<float> heightStart
+				{
+					set { _mod.heightStart = value.value; }
+				}
+
+                // Where the height ends
+				[ParserTarget("heightEnd", optional = true)]
+				private NumericParser<float> heightEnd
+				{
+					set { _mod.heightEnd = value.value; }
+				}
+
+				// The deformity of the simplex terrain
+				[ParserTarget("deformity", optional = true)]
+				private NumericParser<float> deformity
+				{
+					set { _mod.deformity = value.value; }
+				}
+
+				// The frequency of the simplex terrain
+				[ParserTarget("frequency", optional = true)]
+				private NumericParser<float> frequency
+				{
+					set { _mod.frequency = value.value; }
+				}
+
+                // Octaves of the simplex height
+				[ParserTarget("octaves", optional = true)]
+				private NumericParser<int> octaves
+				{
+					set { _mod.octaves = value.value; }
+				}
+
+                // Persistence of the simplex height
+				[ParserTarget("persistance", optional = true)]
+				private NumericParser<float> persistance
+				{
+					set { _mod.persistance = value.value; }
+				}
+
+                // The seed of the simplex height
+				[ParserTarget("seed", optional = true)]
+				private NumericParser<int> seed
+				{
+					set { _mod.seed = value.value; }
+				}
+
+                // lacunarity
+				[ParserTarget("lacunarity", optional = true)]
+				private NumericParser<float> lacunarity
+				{
+					set { _mod.lacunarity = value.value; }
+				}
+
+                // mode
+				[ParserTarget("mode", optional = true)]
+				private EnumParser<LibNoise.Unity.QualityMode> mode
+				{
+					set { _mod.mode = value.value; }
+				}
+
+                // mode
+				[ParserTarget("noiseType", optional = true)]
+				private EnumParser<PQSMod_VertexHeightNoiseVertHeightCurve.NoiseType> noiseType
+				{
+					set { _mod.noiseType = value.value; }
+				}
+
+				void IParserEventSubscriber.Apply(ConfigNode node)
+				{
+
+				}
+
+				void IParserEventSubscriber.PostApply(ConfigNode node)
+				{
+
+				}
+
+                public VertexHeightNoiseVertHeightCurve()
+				{
+					// Create the base mod
+                    GameObject modObject = new GameObject("VertexHeightNoiseVertHeightCurve");
+					modObject.transform.parent = Utility.Deactivator;
+                    _mod = modObject.AddComponent<PQSMod_VertexHeightNoiseVertHeightCurve>();
+					base.mod = _mod;
+				}
+			}
+		}
+	}
+}
+
