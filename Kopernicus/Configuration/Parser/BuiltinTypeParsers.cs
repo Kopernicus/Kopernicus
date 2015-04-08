f58d312eaa8b2f0f74685217e5440c27ddc8e73b
Gravitasi <Gravitasi@users.noreply.github.com>
2015-04-08 22:17:00 +0700

    Fixed a constructor bug on some type parser.

--- Kopernicus/Configuration/Parser/BuiltinTypeParsers.cs
+++ Kopernicus/Configuration/Parser/BuiltinTypeParsers.cs
@@ -449,6 +449,12 @@ namespace Kopernicus
 			// We don't use this
 			void IParserEventSubscriber.PostApply(ConfigNode node) { }
 
+			// Default constructor
+			public AnimationCurveParser ()
+			{
+				this.curve = null;
+			}
+
 			// Construct this fine object
 			public AnimationCurveParser (AnimationCurve curve = null)
 			{
@@ -514,6 +520,12 @@ namespace Kopernicus
 
 			void IParserEventSubscriber.Apply(ConfigNode node) { }
 			void IParserEventSubscriber.PostApply(ConfigNode node) { }
+
+			// Default constructor
+			public PhysicsMaterialParser()
+			{
+				this.material = null;
+			}
 
 			// Initializing constructor
 			public PhysicsMaterialParser (PhysicMaterial material = null)
