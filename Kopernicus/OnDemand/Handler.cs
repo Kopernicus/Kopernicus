ed93d63ea059f7c2783897c11c302d3aa587b8b5
NathanKell <nathankell@gmail.com>
2015-05-19 17:17:25 -0400

    Start writing on-demand MapSO

---
+++ Kopernicus/OnDemand/Handler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kopernicus
+{
+    namespace OnDemand
+    {
+        public class Handler
+        {
+            public static Handler Instance
+            {
+                get
+                {
+                    if (_instance == null)
+                        _instance = new Handler();
+                    return _instance;
+                }
+            }
+            private static Handler _instance;
+
+            public static Dictionary<MapSODemand, bool> enabledMaps;
+            public static Dictionary<string, List<MapSODemand>> perBody;
+
+            public Handler()
+            {
+                enabledMaps = new Dictionary<MapSODemand, bool>();
+                perBody = new Dictionary<string, List<MapSODemand>>();
+            }
+        }
+    }
+}
---
+++ Kopernicus/OnDemand/MapSODemand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using KSP;
+using UnityEngine;
+
+namespace Kopernicus
+{
+    namespace OnDemand
+    {
+        public class MapSODemand : MapSO
+        {
+            protected string mapPath;
+            protected bool isLoaded = false;
+
+            public override void CreateMap(MapSO.MapDepth depth, Texture2D tex)
+            {
+                base.CreateMap(depth, tex);
+                isLoaded = true;
+            }
+            public virtual void Unload()
+            {
+                if (!isLoaded)
+                    return;
+
+            }
+        }
+    }
+}
