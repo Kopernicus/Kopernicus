21ec8417fd86208f6cf1e56bc1bc76c13e44a2a4
Thomas <dorianstoll@googlemail.com>
2015-06-12 22:50:14 +0200

    Changes for 0.0.8

--- Kopernicus/Constants/Version.cs
+++ Kopernicus/Constants/Version.cs
@@ -43,7 +43,7 @@ namespace Kopernicus
         public class Version
         {
             // Versioning information
-            private static int[] versionNumber = new int[] { 0, 0, 7 }; 
+            private static int[] versionNumber = new int[] { 0, 0, 8 }; 
             private static bool developmentBuild = false;
             public static string version
             {
