d758db75c009386622e833c218e0aec37a7f55b7
HappyFaceIndustries <creeperweirdo123@gmail.com>
2015-08-02 15:09:59 -0500

    Revert "Fake merge of ExternalLoaders branch."
    This reverts commit 1b2f3dbe80da517f6d0d598cc74b04ba527234a7.

--- Kopernicus/Configuration/Body.cs
+++ Kopernicus/Configuration/Body.cs
@@ -416,76 +416,12 @@ namespace Kopernicus
                     }
                 //}
 
-				// Loads external parser targets
-				LoadExternalParserTargetsRecursive (node);
-
                 // Post gen celestial body
                 Utility.DumpObjectFields(generatedBody.celestialBody, " Celestial Body ");
 
                 if (generatedBody.celestialBody.isHomeWorld)
                     OnDemand.OnDemandStorage.homeworldBody = name;
             }
-
-			private void LoadExternalParserTargetsRecursive(ConfigNode node)
-			{
-				LoadExternalParserTargets (node);
-				foreach(var childNode in node.GetNodes())
-				{
-					LoadExternalParserTargetsRecursive (childNode);
-				}
-			}
-			private void LoadExternalParserTargets(ConfigNode node)
-			{
-				// Look for types in other assemblies with the ExternalParserTarget attribute and the parentNodeName equal to this node's name
-				foreach (AssemblyLoader.LoadedAssembly assembly in AssemblyLoader.loadedAssemblies)
-				{
-					// Only get types implementing IParserEventSubscriber, and extending ExternalParserTargetLoader
-					foreach (Type type in assembly.assembly.GetExportedTypes().Where(t => t.GetInterface("IParserEventSubscriber") != null).Where(t => t.BaseType == typeof(ExternalParserTargetLoader)))
-					{
-						ExternalParserTarget externalAttr = null;
-
-						var attributes = Attribute.GetCustomAttributes(type);
-						foreach (var attr in attributes.Where (a => a.GetType () == typeof(ExternalParserTarget)))
-						{
-							externalAttr = attr as ExternalParserTarget;
-						}
-
-						if (externalAttr != null)
-						{
-							if (node.name != externalAttr.parentNodeName)
-								continue;
-
-							string nodeName = externalAttr.configNodeName;
-							if (nodeName == null)
-							{
-								nodeName = type.Name;
-							}
-
-							if(node.HasNode(externalAttr.configNodeName))
-							{
-								try
-								{
-									Logger.Active.Log("Parsing ExternalTarget " + externalAttr.configNodeName + " in node " + externalAttr.parentNodeName + " from Assembly " + assembly.assembly.FullName);
-									var nodeToLoad = node.GetNode (externalAttr.configNodeName);
-									ExternalParserTargetLoader obj = Activator.CreateInstance (type) as ExternalParserTargetLoader;
-									obj.generatedBody = generatedBody;
-									Parser.LoadObjectFromConfigurationNode (obj, nodeToLoad);
-								}
-								catch(MissingMethodException missingMethod)
-								{
-									Logger.Active.Log ("Failed to load ExternalParserTarget " + externalAttr.configNodeName + " because it does not have a parameterless constructor");
-									Logger.Active.LogException (missingMethod);
-								}
-								catch (Exception exception)
-								{
-									Logger.Active.Log ("Failed to load ExternalParserTarget " + externalAttr.configNodeName + " from node " + externalAttr.parentNodeName);
-									Logger.Active.LogException (exception);
-								}
-							}
-						}
-					}
-				}
-			}
         }
     }
 }
