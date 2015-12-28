e4084875714b6b7cb6298455611f10a7a94dc509
Thomas <dorianstoll@googlemail.com>
2015-12-28 11:38:35 +0100

    0.6.3 FINAL

--- Kopernicus/Constants/Version.cs
+++ Kopernicus/Constants/Version.cs
@@ -40,7 +40,7 @@ namespace Kopernicus
         public class Version
         {
             // Versioning information
-            public const string versionNumber = "0.6.2"; 
+            public const string versionNumber = "0.6.3"; 
 
             // Get a string for the logging
             public static string version
