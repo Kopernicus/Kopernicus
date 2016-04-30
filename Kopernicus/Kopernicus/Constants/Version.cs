5bc2441155ec1633ca397ecfbdd993f1783d0155
Thomas <dorian@tmsp.ga>
2016-04-30 14:31:37 +0200

    Bumps

--- Kopernicus/Kopernicus/Constants/Version.cs
+++ Kopernicus/Kopernicus/Constants/Version.cs
@@ -40,7 +40,7 @@ namespace Kopernicus
         public class Version
         {
             // Versioning information
-            public const string versionNumber = "1.0.2";
+            public const string versionNumber = "1.0.3";
 
             // Get a string for the logging
             public static string version
