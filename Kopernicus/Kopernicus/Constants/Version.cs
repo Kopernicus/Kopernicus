23252d5027cc5614f3e71f2c923b220121a76bd3
Thomas <dorian@tmsp.ga>
2016-05-06 17:28:23 +0200

    1.0.4 - "Copying Squads versioning" Edition

--- Kopernicus/Kopernicus/Constants/Version.cs
+++ Kopernicus/Kopernicus/Constants/Version.cs
@@ -40,7 +40,7 @@ namespace Kopernicus
         public class Version
         {
             // Versioning information
-            public const string versionNumber = "1.0.3";
+            public const string versionNumber = "1.0.4";
 
             // Get a string for the logging
             public static string version
