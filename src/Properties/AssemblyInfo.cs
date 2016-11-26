73ca5b132c4256d69d944cd8b02d8c68bfdb0b95
Dorian Stoll <dorian@tmsp.ga>
2016-11-26 17:57:20 +0100

    Add project from Kopernicus

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kopernicus.Parser")]
+[assembly: AssemblyDescription("A parsing library for ConfigNodes that uses Reflection and Attribution.")]
+[assembly: AssemblyProduct("Kopernicus.Parser")]
+[assembly: AssemblyCopyright("Copyright © Nathaniel R. Lewis (Teknoman117), Thomas P. 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f9968d87-83da-453c-8e28-b632367eacd3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
