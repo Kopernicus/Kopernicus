993f646ab4726e911c2b3f9e16b9a0a5c609c96f
Dorian Stoll <dorian@tmsp.io>
2017-05-27 13:07:07 +0200

    Fix displayName resetting

--- Kopernicus/Kopernicus/Configuration/Body.cs
+++ Kopernicus/Kopernicus/Configuration/Body.cs
@@ -166,7 +166,6 @@ namespace Kopernicus
                     // Patch the game object names in the template
                     generatedBody.name = name;
                     generatedBody.celestialBody.bodyName = name;
-                    generatedBody.celestialBody.bodyDisplayName = name;
                     generatedBody.celestialBody.transform.name = name;
                     generatedBody.celestialBody.bodyTransform.name = name;
                     generatedBody.scaledVersion.name = name;
@@ -180,7 +179,7 @@ namespace Kopernicus
                     }
 
                     // If we've changed the name, reset use_The_InName
-                    if (generatedBody.name != template.body.name)
+                    if (generatedBody.name != template.originalBody.celestialBody.bodyName)
                     {
                         generatedBody.celestialBody.bodyDisplayName = generatedBody.celestialBody.bodyName;
                     }
