5e97f4728f047e2a95ed88a5aa4739d3147e0d26
thomas-jenkins <jenkins@tmsp.io>
2018-03-31 13:39:30 +0200

    Kopernicus 1.4.2-1 - "By reading this you are accepting my EULA - Edition"

--- src/Kopernicus/Constants/CompatibilityChecker.cs
+++ src/Kopernicus/Constants/CompatibilityChecker.cs
@@ -48,9 +48,9 @@ namespace Kopernicus
     {
         // Compatible version
         internal const Int32 version_major = 1;
-        internal const Int32 version_minor = 3;
-        internal const Int32 Revision = 1;
-        internal const Int32 Kopernicus = 7;
+        internal const Int32 version_minor = 4;
+        internal const Int32 Revision = 2;
+        internal const Int32 Kopernicus = 1;
 
         public static Boolean IsCompatible()
         {
