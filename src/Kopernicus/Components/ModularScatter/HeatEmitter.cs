96d5e92710d9ea85fd14129d998d19d53933a1b1
Dorian Stoll <dorian.stoll@tmsp.io>
2019-05-14 21:09:08 +0200

    Add a module that makes scatter objects emit heat
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

---
+++ src/Kopernicus/Components/ModularScatter/HeatEmitter.cs
@@ -0,0 +1,86 @@
+﻿/**
+ * Kopernicus Planetary System Modifier
+ * ------------------------------------------------------------- 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * This library is intended to be used as a plugin for Kerbal Space Program
+ * which is copyright of TakeTwo Interactive. Your usage of Kerbal Space Program
+ * itself is governed by the terms of its EULA, not the license above.
+ * 
+ * https://kerbalspaceprogram.com
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Kopernicus.Components.ModularComponentSystem;
+using UnityEngine;
+
+namespace Kopernicus.Components.ModularScatter
+{
+    /// <summary>
+    /// A Scatter Component that emits heat onto the active vessel
+    /// </summary>
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    public class HeatEmitterComponent : IComponent<ModularScatter>
+    {
+        /// <summary>
+        /// How much heat should be applied at a certain distance
+        /// </summary>
+        public FloatCurve HeatCurve;
+        
+        /// <summary>
+        /// The amount of heat that gets added to the active vessel every n seconds
+        /// </summary>
+        public Double HeatRate;
+
+        /// <summary>
+        /// How many seconds should pass between applying the heat rate to the ship.
+        /// </summary>
+        public Double HeatInterval = 0.05d;
+
+        /// <summary>
+        /// Gets executed every frame and checks if a Kerbal is within the range of the scatter object
+        /// </summary>
+        void IComponent<ModularScatter>.Update(ModularScatter system)
+        {
+            // Check for an active vessel
+            if (!FlightGlobals.ActiveVessel || !FlightGlobals.ActiveVessel.packed)
+            {
+                return;
+            }
+            
+            // Get the distance between the active vessel and ourselves
+            Single distance = Vector3.Distance(system.transform.position, FlightGlobals.ship_position);
+            Double heat = HeatRate / HeatInterval * HeatCurve.Evaluate(distance);
+            
+            // Apply the heat to all parts of the active vessel
+            for (Int32 i = 0; i < FlightGlobals.ActiveVessel.Parts.Count; i++) 
+            {
+                FlightGlobals.ActiveVessel.Parts[i].temperature += heat;
+            }
+        }
+
+        void IComponent<ModularScatter>.Apply(ModularScatter system)
+        {
+            // We don't use this
+        }
+
+        void IComponent<ModularScatter>.PostApply(ModularScatter system)
+        {
+            // We don't use this
+        }
+    }
+}
---
+++ src/Kopernicus/Configuration/ModularScatterLoader/HeatEmitter.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * Kopernicus Planetary System Modifier
+ * ------------------------------------------------------------- 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * 
+ * This library is intended to be used as a plugin for Kerbal Space Program
+ * which is copyright of TakeTwo Interactive. Your usage of Kerbal Space Program
+ * itself is governed by the terms of its EULA, not the license above.
+ * 
+ * https://kerbalspaceprogram.com
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Kopernicus.Components.ModularScatter;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.ConfigParser.Enumerations;
+using Kopernicus.Configuration.Parsing;
+using Kopernicus.UI;
+
+namespace Kopernicus.Configuration.ModularScatterLoader
+{
+    /// <summary>
+    /// The loader for heat emitting scatter objects
+    /// </summary>
+    [RequireConfigType(ConfigType.Node)]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class HeatEmitter : ComponentLoader<ModularScatter, HeatEmitterComponent>
+    {
+        // The average heat emitted by the scatter
+        [ParserTarget("heat")]
+        [KittopiaDescription("The average heat emitted by the scatter.")]
+        public NumericParser<Double> Heat
+        {
+            get { return Value.HeatRate; }
+            set { Value.HeatRate = value; }
+        }
+
+        // How many seconds pass between applying the heat to a vessel
+        [ParserTarget("interval")]
+        [KittopiaDescription("How many seconds pass between applying the heat to a vessel.")]
+        public NumericParser<Double> Interval
+        {
+            get { return Value.HeatInterval; }
+            set { Value.HeatInterval = value; }
+        }
+        
+        // Controls the how much of the average heat gets applied at a certain distance
+        [ParserTarget("DistanceCurve")]
+        [KittopiaDescription("Controls the how much of the average heat gets applied at a certain distance.")]
+        public FloatCurveParser DistanceCurve
+        {
+            get { return Value.HeatCurve; }
+            set { Value.HeatCurve = value; }
+        }
+    }
+}
