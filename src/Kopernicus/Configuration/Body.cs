4eb9a40b9391a6f3f6ed900cb2711b280e13c335
Sigma88 <sigmamodexpansions@outlook.com>
2019-07-11 14:14:35 +0200

    Update Body.cs

--- src/Kopernicus/Configuration/Body.cs
+++ src/Kopernicus/Configuration/Body.cs
@@ -94,7 +94,10 @@ namespace Kopernicus.Configuration
                 CelestialBody.Set("barycenter", value.Value);
 
                 // Reuse the selectable option, to reduce the amount of boilerplate code we need.
-                CelestialBody.Set("selectable", !value.Value);
+                if (value.Value)
+                {
+                    CelestialBody.Set("selectable", !value.Value);
+                }
             }
         }
 
