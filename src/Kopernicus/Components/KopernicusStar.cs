cc72b8a493f24099f73efc63ff8898e659ecd278
Raymond Beehler <21GunSoftware@comcast.net>
2020-07-20 14:43:04 -0700

    Add .referencebody to thermal calculations
    This should fix the last commit that broke thermal calculations.  We are testing this in my bleeding edge branch before releasing a buggy release by accident, again.

--- src/Kopernicus/Components/KopernicusStar.cs
+++ src/Kopernicus/Components/KopernicusStar.cs
@@ -283,7 +283,7 @@ namespace Kopernicus.Components
             // Get Thermal Stats
             if (vessel.mainBody.atmosphere)
             {
-                if (sun == GetBodyReferencing(vessel.mainBody))
+                if (sun == GetBodyReferencing(vessel.mainBody.referenceBody))
                 {
                     FlightIntegrator FI = vessel.GetComponent<FlightIntegrator>();
                     vessel.mainBody.GetAtmoThermalStats(true, sun, sunVector, Vector3d.Dot(sunVector, vessel.upAxis), vessel.upAxis, vessel.altitude, out FI.atmosphereTemperatureOffset, out FI.bodyEmissiveFlux, out FI.bodyAlbedoFlux);
