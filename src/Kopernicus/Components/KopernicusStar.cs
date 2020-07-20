6965a77e88dc40e2613553262422aeaa14e653bc
Raymond Beehler <21GunSoftware@comcast.net>
2020-07-20 11:50:06 -0700

    Integrate @democat3457's fixes from Prestja/Kopernicus PR#20 (Thanks man!)

--- src/Kopernicus/Components/KopernicusStar.cs
+++ src/Kopernicus/Components/KopernicusStar.cs
@@ -283,7 +283,7 @@ namespace Kopernicus.Components
             // Get Thermal Stats
             if (vessel.mainBody.atmosphere)
             {
-                if (sun == GetBodyReferencing(vessel.mainBody) && !vessel.mainBody.isStar)
+                if (sun == GetBodyReferencing(vessel.mainBody))
                 {
                     FlightIntegrator FI = vessel.GetComponent<FlightIntegrator>();
                     vessel.mainBody.GetAtmoThermalStats(true, sun, sunVector, Vector3d.Dot(sunVector, vessel.upAxis), vessel.upAxis, vessel.altitude, out FI.atmosphereTemperatureOffset, out FI.bodyEmissiveFlux, out FI.bodyAlbedoFlux);
@@ -358,7 +358,7 @@ namespace Kopernicus.Components
         /// </summary>
         public static CelestialBody GetBodyReferencing(CelestialBody body)
         {
-            while (body?.orbit?.referenceBody != null)
+            while (body?.orbit?.referenceBody != null && !body.orbit.referenceBody.isStar)
             {
                 body = body.orbit.referenceBody;
             }
