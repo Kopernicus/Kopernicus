73c10ec58b2652a7ad4f2dd2ecef32002ba2675a
Raymond Beehler <21GunSoftware@comcast.net>
2020-07-26 14:57:51 -0700

    Add support for clamping wrapping doubles of EC in case it happens (it does in some extreme instances).

--- src/Kopernicus/Components/KopernicusSolarPanels.cs
+++ src/Kopernicus/Components/KopernicusSolarPanels.cs
@@ -162,7 +162,7 @@ namespace Kopernicus.Components
                         SP.sunAOA = totalAoA;
                         SP.sunAOA /= _relativeSunAoa ? KopernicusStar.Stars.Count : 1;
                         //Clamping to EC-limit
-                        if (totalFlow > SP.chargeRate)
+                        if (totalFlow > SP.chargeRate || (totalFlow < 0))
                         {
                             _totalFlow = SP.chargeRate;
                             totalFlow = SP.chargeRate;
