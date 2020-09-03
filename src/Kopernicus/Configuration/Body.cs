16b3605e029137b195a15227ae071114d21d5c1c
R-T-B <21GunSoftware@comcast.net>
2020-09-03 15:20:39 -0700

    Generalize this section of code.

--- src/Kopernicus/Configuration/Body.cs
+++ src/Kopernicus/Configuration/Body.cs
@@ -295,11 +295,18 @@ namespace Kopernicus.Configuration
                 {
                     if ((!Name.Equals("Jool")) || (Name.Equals("Jool") && (Template.Body.celestialBody.Radius > 6000000))) // This is a Jool-clone, or resized Jool.  We have to handle it special.
                     {
-                        //Remove Gas Giant shaders for compatability
-                        GasGiantMaterialControls GGMC = GeneratedBody.scaledVersion.GetComponent<GasGiantMaterialControls>();
-                        MaterialBasedOnGraphicsSetting MBOGS = GeneratedBody.scaledVersion.GetComponent<MaterialBasedOnGraphicsSetting>();
-                        GameObject.DestroyImmediate(GGMC);
-                        GameObject.DestroyImmediate(MBOGS);
+                        try
+                        {
+                            //Remove Gas Giant shaders for compatability
+                            GasGiantMaterialControls GGMC = GeneratedBody.scaledVersion.GetComponent<GasGiantMaterialControls>();
+                            MaterialBasedOnGraphicsSetting MBOGS = GeneratedBody.scaledVersion.GetComponent<MaterialBasedOnGraphicsSetting>();
+                            GameObject.DestroyImmediate(GGMC);
+                            GameObject.DestroyImmediate(MBOGS);
+                        }
+                        catch
+                        {
+                            //we don't care that it did not work, probably 1.9.1
+                        }
                     }
                 }
                 // Create accessors
