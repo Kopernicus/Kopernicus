2672de0acd81b0ec71d201ae8e0c642503490651
R-T-B <21GunSoftware@comcast.net>
2020-09-30 04:29:44 -0700

    Release 35.

--- src/Kopernicus/Constants/CompatibilityChecker.cs
+++ src/Kopernicus/Constants/CompatibilityChecker.cs
@@ -51,7 +51,7 @@ namespace Kopernicus.Constants
         internal const Int32 VERSION_MAJOR = 1;
         internal const Int32 VERSION_MINOR = 10;
         internal const Int32 REVISION = 1;
-        internal const Int32 KOPERNICUS = 34;
+        internal const Int32 KOPERNICUS = 35;
 
         public static Boolean IsCompatible()
         {
