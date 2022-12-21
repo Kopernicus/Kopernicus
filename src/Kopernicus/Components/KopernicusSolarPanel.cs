e89c854fadecf3e51cf9b21708179f96c5ab7ee1
R-T-B <21GunSoftware@comcast.net>
2022-12-20 17:07:14 -0800

    Localization fix.

--- src/Kopernicus/Components/KopernicusSolarPanel.cs
+++ src/Kopernicus/Components/KopernicusSolarPanel.cs
@@ -1,4 +1,4 @@
-﻿/**
+﻿/**U
  * Kopernicus Planetary System Modifier
  * ------------------------------------------------------------- 
  * This library is free software; you can redistribute it and/or
