b99251786a1ddd68ab7d15b4dad55210510bf6b1
R-T-B <21GunSoftware@comcast.net>
2023-05-29 16:42:52 -0700

    Release-170 RC1.  Restore the biome sampler.

--- src/Kopernicus/Properties/AssemblyInfo.cs
+++ src/Kopernicus/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using Kopernicus.RuntimeUtility;
 [assembly: KSPAssemblyDependency("Kopernicus.Parser", 1, 0)]
 [assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.12.169.0")]
+[assembly: AssemblyFileVersion("1.12.170.2")]
 [assembly: LogAggregator("GameData/ModuleManager.ConfigCache")]
 [assembly: LogAggregator("Logs/Kopernicus/")]
 [assembly: LogAggregator("KSP.log")] 
