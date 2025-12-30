471418134e06e832ecd555060a787681fc142c9c
Sean Lynch <phantom@lynches.ca>
2025-12-30 15:34:16 -0500

    Ensure that configurations and platforms are correct

--- src/Kopernicus/Constants/CompatibilityChecker.cs
+++ src/Kopernicus/Constants/CompatibilityChecker.cs
@@ -51,7 +51,7 @@ namespace Kopernicus.Constants
 #if !KSP_VERSION_1_8
         internal const string VERSION_MINOR = "12";
 #else
-        internal String Int32 VERSION_MINOR = "8";
+        internal const string VERSION_MINOR = "8";
 #endif
         internal const string VERSION_MINOR_LOWER_LIMIT = "8";
         internal const string REVISION = "1";
