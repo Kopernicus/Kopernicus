c4556d1dc111cea0c8eb137e3cb8403b180274d2
Sean Lynch <phantom@lynches.ca>
2025-12-30 15:15:35 -0500

    Modernize build system
    This switches the project files to be a sdk-style project, using
    KSPBuildTools to take care of the details of depending on the KSP dlls.
    
    The developer-facing changes for this are:
    * You now only need to update the release number once in
      Kopernicus.csproj. The code and version files will get updated based
      on that constant.
    * It is no longer necessary to manually add new source files to a list
      in Kopernicus.csproj, all files within are added explicitly. The only
      exception are those in the ShadowMan unity project, which I have
      explicitly excluded.
    * It is no longer necessary to do the first build through visual studio.
      dotnet build (and visual studio) will be able to immediately build the
      project and restore things.
    
    Other internal changes are:
    * Harmony is now vendored under dependencies. I wasn't able to figure
      out the right way to prevent if from being copied to the build
      directory, and a vendored dependency was the easiest way to manage
      that.

--- src/Kopernicus/Properties/AssemblyInfo.cs
+++ src/Kopernicus/Properties/AssemblyInfo.cs
@@ -1,22 +1,13 @@
 ﻿using System.Reflection;
+using Kopernicus;
 using Kopernicus.Constants;
 using Kopernicus.RuntimeUtility;
 
-[assembly: AssemblyTitle("Kopernicus")]
-[assembly: AssemblyDescription("Planetary System Modifier for Kerbal Space Program")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kopernicus Project")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (C) Kopernicus Project")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: KSPAssembly("Kopernicus", 1, 0)]
 [assembly: KSPAssemblyDependency("Kopernicus.Parser", 1, 0)]
 [assembly: KSPAssemblyDependency("ModularFlightIntegrator", 1, 0)]
 [assembly: KSPAssemblyDependency("0Harmony", 0, 0)]
 [assembly: KSPAssemblyDependency("KSPTextureLoader", 0, 0, 7)]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.12." + CompatibilityChecker.KOPERNICUS + ".0")]
 [assembly: LogAggregator("GameData/ModuleManager.ConfigCache")]
 [assembly: LogAggregator("Logs/Kopernicus/")]
 [assembly: LogAggregator("KSP.log")]
