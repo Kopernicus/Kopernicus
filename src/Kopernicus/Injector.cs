e7fbe8c16741d6994d441cf0dc27845b140fdf21
Sean Lynch <phantom@lynches.ca>
2026-01-19 23:38:08 -0500

    Avoid unnecessarily loading heightmaps in main menu for most bodies  (#748)
    * Proactively unload unused textures during PSystemSetup
    
    During config PSystemSetup kopernicus first loads a bunch of textures,
    and then Start calls for all the PQS instances end up loading every
    single heightmap. This frame ends up using more memory than literally
    any other time.
    
    By proactively unloading textures after config parsing, and again after
    each call to PQS.Start, we can keep the maximum memory usage to
    something reasonable.
    
    * Avoid calling GetSurfaceHeight for most bodies in PQS.Start
    
    PQS.Start calls GetSurfaceHeight for every body in the system in order
    to determine if the planet needs to be subdivided for the main menu
    scene. This is a waste of time for most planets, since they will much
    too far away to even be visiable, let alone have PQS active.
    
    This patches UpdateVisual to only actually call GetSurfaceHeight if the
    target (i.e. the current camera) is close enough that the planet might
    actually have PQS attempt to subdivide it. It restricts this change to
    only apply during the main menu scene so as not to mess with anything
    during the rest of the game.

--- src/Kopernicus/Injector.cs
+++ src/Kopernicus/Injector.cs
@@ -27,6 +27,7 @@ using HarmonyLib;
 using Kopernicus.ConfigParser;
 using Kopernicus.Configuration;
 using Kopernicus.Constants;
+using KSPTextureLoader;
 using SentinelMission;
 using System;
 using System.Collections.Generic;
@@ -184,6 +185,11 @@ namespace Kopernicus
 
                 // Open the Warning popup
                 DisplayWarning();
+            }
+            finally
+            {
+                // Attempt to reduce memory usage by immediately unloading unused textures
+                TextureLoader.ImmediatelyDestroyUnusedTextures();
             }
         }
 
@@ -810,6 +816,46 @@ namespace Kopernicus
             outerBody = dictionary[list[list.Count - 1]];
             __result = false;
             return false;
+        }
+    }
+
+    [HarmonyDebug]
+    [HarmonyPatch(typeof(PQS), "UpdateVisual")]
+    internal static class PQS_UpdateVisual
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var oldMethod = SymbolExtensions.GetMethodInfo<PQS>(pqs => pqs.GetSurfaceHeight(Vector3d.zero));
+            var newMethod = SymbolExtensions.GetMethodInfo<PQS>(pqs => GetSurfaceHeightIfNecessary(pqs, default));
+
+            var matcher = new CodeMatcher(instructions);
+            matcher
+                .MatchStartForward(new CodeMatch(OpCodes.Call, oldMethod))
+                .ThrowIfInvalid("Could not find call to PQS::GetSurfaceHeight")
+                .SetInstruction(new CodeInstruction(OpCodes.Call, newMethod));
+
+            return matcher.Instructions();
+        }
+
+        // PQS.Start calls GetSurfaceHeight for every body in the system, this includes ones that
+        // have no conceivable chance of being near the camera. This ends up being incredibly
+        // slow because it is necessary to load the heightmaps for all the planets in the system.
+        //
+        // This is a modified version of GetSurfaceHeight that skips returning the height if we're
+        // still in the loading screen and the planet is far enough away that it wouldn't be
+        // subdivided anyway.
+        //
+        // UpdateVisual gets called every frame during regular gameplay anyways, so this has no
+        // effect on anything outside of the main menu scene.
+        static double GetSurfaceHeightIfNecessary(PQS pqs, Vector3d radialVector)
+        {
+            if (HighLogic.LoadedScene != GameScenes.LOADING)
+                return pqs.GetSurfaceHeight(radialVector);
+
+            if (pqs.targetAltitude <= pqs.maxDetailDistance * pqs.radius)
+                return pqs.GetSurfaceHeight(radialVector);
+
+            return 0.0;
         }
     }
 }
