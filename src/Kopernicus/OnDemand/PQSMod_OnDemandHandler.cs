b4a31672e9ed4b0246a0b2088e712d5c20ae4bc6
Phantomical <phantom@lynches.ca>
2026-03-14 22:11:22 -0700

    Use a coroutine to track when the body is loaded and should unload

--- src/Kopernicus/OnDemand/PQSMod_OnDemandHandler.cs
+++ src/Kopernicus/OnDemand/PQSMod_OnDemandHandler.cs
@@ -24,6 +24,7 @@
  */
 
 using System;
+using System.Collections;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using Stopwatch = System.Diagnostics.Stopwatch;
@@ -35,6 +36,9 @@ namespace Kopernicus.OnDemand
     {
         // A minimum frame delay so that we don't unload if there is a large lag spike.
         const int UnloadFrameDelay = 30;
+        const int UpdateInterval = 5;
+
+        static readonly WaitForEndOfFrame WaitForEndOfFrame = new();
 
         // Delayed unload
         // If non-zero the textures will be unloaded once the timer exceeds the value
@@ -44,7 +48,9 @@ namespace Kopernicus.OnDemand
         private int _lastUpdateFrame = 0;
 
         // State
-        private bool _isLoaded;
+        private Coroutine _coroutine;
+
+        private bool IsLoaded => _coroutine is not null;
 
         long GetUnloadTime()
         {
@@ -74,10 +80,10 @@ namespace Kopernicus.OnDemand
 
         internal void Activate()
         {
-            if (_isLoaded)
+            if (IsLoaded)
             {
                 // Refresh unload time at most once every 5 frames per OnDemandHandler.
-                if (_lastUpdateFrame + 5 >= Time.frameCount)
+                if (_lastUpdateFrame + UpdateInterval >= Time.frameCount)
                     return;
             }
             else
@@ -87,7 +93,7 @@ namespace Kopernicus.OnDemand
                 // as a result of that will get unloaded eventually instead of
                 // hanging around forever.
 
-                _isLoaded = true;
+                _coroutine = StartCoroutine(UnloadWatcher());
             }
 
             UpdateUnloadTime();
@@ -105,48 +111,51 @@ namespace Kopernicus.OnDemand
             Debug.Log($"[OD] Enabling Body {sphere.name}");
         }
 
-        // Disabling
-        public override void OnSphereInactive()
-        {
-            // Don't update, if the Injector is still running
-            if (!_isLoaded)
-                return;
-
-            // Set the time at which to unload
-            _unloadTime = GetUnloadTime();
-        }
+        public override void OnQuadPreBuild(PQ quad) => Activate();
 
         public override void OnVertexBuildHeight(PQS.VertexBuildData data) =>
             Activate();
 
-        private void LateUpdate()
+        IEnumerator UnloadWatcher()
         {
-            // If we aren't loaded or we're not wanting to unload then do nothing
-            if (!_isLoaded || _unloadTime == 0)
-                return;
+            using var guard = new ClearCoroutineGuard(this);
 
-            // If we are the currently active body, do not unload.
-            if (sphere.IsNotNullOrDestroyed() && FlightGlobals.ActiveVessel.IsNotNullOrDestroyed())
+            while (true)
             {
-                var activeBody = FlightGlobals.ActiveVessel.mainBody;
-                if (activeBody.RefEquals(sphere.GetCelestialBody()))
-                    return;
+                yield return WaitForEndOfFrame;
+
+                // We will never unload ourselves while the PQS sphere is loaded.
+                if (sphere.isActive)
+                    continue;
+
+                // If we are the currently active body, do not unload.
+                if (sphere.IsNotNullOrDestroyed() && FlightGlobals.ActiveVessel.IsNotNullOrDestroyed())
+                {
+                    var activeBody = FlightGlobals.ActiveVessel.mainBody;
+                    if (activeBody.RefEquals(sphere.GetCelestialBody()))
+                        continue;
+                }
+
+                // We stay loadeed for at least UnloadFrameDelay frames, so that
+                // lag spikes do not result in unloading things unexpectedly.
+                if (Time.frameCount <= _unloadFrame)
+                    continue;
+
+                // Otherwise we need to wait at least OnDemandUnloadDelay before
+                // we unload anything.
+                if (Stopwatch.GetTimestamp() <= _unloadTime)
+                    continue;
+
+                break;
             }
 
-            // Check if the necessary number of frames have passed to prevent
-            // lag spikes from causing us to unload textures.
-            if (Time.frameCount <= _unloadFrame)
-                return;
-
-            // If we're past the unload time then unload
-            if (Stopwatch.GetTimestamp() <= _unloadTime)
-                return;
-
-            // Disable the maps
             if (OnDemandStorage.DisableBodyPqs(sphere.name))
                 Debug.Log($"[OD] Disabling Body {sphere.name}");
+        }
 
-            _isLoaded = false;
+        struct ClearCoroutineGuard(PQSMod_OnDemandHandler handler) : IDisposable
+        {
+            public void Dispose() => handler._coroutine = null;
         }
     }
 }
