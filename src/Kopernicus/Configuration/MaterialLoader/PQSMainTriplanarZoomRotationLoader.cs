aef0473274abc89841b8392e66a0b8f8625114a0
Phantomical <phantom@lynches.ca>
2026-05-03 00:13:11 -0700

    Add support for the PQS Main Shader - Triplanar Zoom Rotation shader
    Kerbin's stock high-quality terrain material binds
    "Terrain/PQS/PQS Main Shader - Triplanar Zoom Rotation", which has no
    match in the existing wrapper set: it carries low/high bump maps and
    omits the steep fade range and near-tiling that "PQS Triplanar Zoom
    Rotation" has.
    
    Add a dedicated wrapper, loader, and MainTriplanarZoomRotation entry to
    NewShaderSurfaceMaterialType so PQSLoader's shader -> type and
    type -> loader switches resolve it instead of throwing.

---
+++ src/Kopernicus/Configuration/MaterialLoader/PQSMainTriplanarZoomRotationLoader.cs
@@ -0,0 +1,440 @@
+/**
+ * Kopernicus Planetary System Modifier
+ * -------------------------------------------------------------
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ *
+ * This library is intended to be used as a plugin for Kerbal Space Program
+ * which is copyright of TakeTwo Interactive. Your usage of Kerbal Space Program
+ * itself is governed by the terms of its EULA, not the license above.
+ *
+ * https://kerbalspaceprogram.com
+ */
+
+using System;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.ConfigParser.Enumerations;
+using Kopernicus.Configuration.MaterialLoader.Parsing;
+using Kopernicus.Configuration.Parsing;
+using UnityEngine;
+
+namespace Kopernicus.Configuration.MaterialLoader
+{
+    [RequireConfigType(ConfigType.Node)]
+    public class PQSMainTriplanarZoomRotationLoader : BaseMaterialLoader
+    {
+        private const String SHADER_NAME = "Terrain/PQS/PQS Main Shader - Triplanar Zoom Rotation";
+        private static readonly Shader Shader = Shader.Find(SHADER_NAME);
+        public static bool UsesSameShader(Material m) => m != null && m.shader.name == SHADER_NAME;
+
+        // Factor, default = 10
+        [ParserTarget("factor")]
+        public NumericParser<float> FactorSetter
+        {
+            get => GetFloat("_factor");
+            set => SetFloat("_factor", value);
+        }
+
+        // Factor Blend Width, default = 0.1
+        [ParserTarget("factorBlendWidth")]
+        public NumericParser<float> FactorBlendWidthSetter
+        {
+            get => GetFloat("_factorBlendWidth");
+            set => SetFloat("_factorBlendWidth", value);
+        }
+
+        // Factor Rotation, default = 30
+        [ParserTarget("factorRotation")]
+        public NumericParser<float> FactorRotationSetter
+        {
+            get => GetFloat("_factorRotation");
+            set => SetFloat("_factorRotation", value);
+        }
+
+        // Saturation, default = 1
+        [ParserTarget("saturation")]
+        public NumericParser<float> SaturationSetter
+        {
+            get => GetFloat("_saturation");
+            set => SetFloat("_saturation", value);
+        }
+
+        // Contrast, default = 1
+        [ParserTarget("contrast")]
+        public NumericParser<float> ContrastSetter
+        {
+            get => GetFloat("_contrast");
+            set => SetFloat("_contrast", value);
+        }
+
+        // Colour Unsaturation (A = Factor), default = (1,1,1,0)
+        [ParserTarget("tintColor")]
+        public ColorParser TintColorSetter
+        {
+            get => GetColor("_tintColor");
+            set => SetColor("_tintColor", value);
+        }
+
+        // Specular Color, default = (0.2,0.2,0.2,0.2)
+        [ParserTarget("specularColor")]
+        public ColorParser SpecularColorSetter
+        {
+            get => GetColor("_specularColor");
+            set => SetColor("_specularColor", value);
+        }
+
+        // Brightness, default = 2
+        [ParserTarget("albedoBrightness")]
+        public NumericParser<float> AlbedoBrightnessSetter
+        {
+            get => GetFloat("_albedoBrightness");
+            set => SetFloat("_albedoBrightness", value);
+        }
+
+        // Steep Blend, default = 1
+        [ParserTarget("steepPower")]
+        public NumericParser<float> SteepPowerSetter
+        {
+            get => GetFloat("_steepPower");
+            set => SetFloat("_steepPower", value);
+        }
+
+        // Steep Texture, default = "white" { }
+        [ParserTarget("steepTex")]
+        public MaterialTextureParser SteepTexSetter
+        {
+            get => null;
+            set => SetTexture("_steepTex", value);
+        }
+
+        [ParserTarget("steepTexScale")]
+        public Vector2Parser SteepTexScaleSetter
+        {
+            get => GetTextureScale("_steepTex");
+            set => SetTextureScale("_steepTex", value);
+        }
+
+        [ParserTarget("steepTexOffset")]
+        public Vector2Parser SteepTexOffsetSetter
+        {
+            get => GetTextureOffset("_steepTex");
+            set => SetTextureOffset("_steepTex", value);
+        }
+
+        // Steep Tiling, default = 1
+        [ParserTarget("steepTiling")]
+        public NumericParser<float> SteepTilingSetter
+        {
+            get => GetFloat("_steepTiling");
+            set => SetFloat("_steepTiling", value);
+        }
+
+        // Steep Bump Map, default = "bump" { }
+        [ParserTarget("steepBumpMap")]
+        public MaterialTextureParser SteepBumpMapSetter
+        {
+            get => null;
+            set => SetTexture("_steepBumpMap", value);
+        }
+
+        [ParserTarget("steepBumpMapScale")]
+        public Vector2Parser SteepBumpMapScaleSetter
+        {
+            get => GetTextureScale("_steepBumpMap");
+            set => SetTextureScale("_steepBumpMap", value);
+        }
+
+        [ParserTarget("steepBumpMapOffset")]
+        public Vector2Parser SteepBumpMapOffsetSetter
+        {
+            get => GetTextureOffset("_steepBumpMap");
+            set => SetTextureOffset("_steepBumpMap", value);
+        }
+
+        // Steep Bump Tiling, default = 1
+        [ParserTarget("steepBumpTiling")]
+        public NumericParser<float> SteepBumpTilingSetter
+        {
+            get => GetFloat("_steepBumpTiling");
+            set => SetFloat("_steepBumpTiling", value);
+        }
+
+        // Low Texture, default = "white" { }
+        [ParserTarget("lowTex")]
+        public MaterialTextureParser LowTexSetter
+        {
+            get => null;
+            set => SetTexture("_lowTex", value);
+        }
+
+        [ParserTarget("lowTexScale")]
+        public Vector2Parser LowTexScaleSetter
+        {
+            get => GetTextureScale("_lowTex");
+            set => SetTextureScale("_lowTex", value);
+        }
+
+        [ParserTarget("lowTexOffset")]
+        public Vector2Parser LowTexOffsetSetter
+        {
+            get => GetTextureOffset("_lowTex");
+            set => SetTextureOffset("_lowTex", value);
+        }
+
+        // Low Tiling, default = 100000
+        [ParserTarget("lowTiling")]
+        public NumericParser<float> LowTilingSetter
+        {
+            get => GetFloat("_lowTiling");
+            set => SetFloat("_lowTiling", value);
+        }
+
+        // Low Bump Map, default = "bump" { }
+        [ParserTarget("lowBumpMap")]
+        public MaterialTextureParser LowBumpMapSetter
+        {
+            get => null;
+            set => SetTexture("_lowBumpMap", value);
+        }
+
+        [ParserTarget("lowBumpMapScale")]
+        public Vector2Parser LowBumpMapScaleSetter
+        {
+            get => GetTextureScale("_lowBumpMap");
+            set => SetTextureScale("_lowBumpMap", value);
+        }
+
+        [ParserTarget("lowBumpMapOffset")]
+        public Vector2Parser LowBumpMapOffsetSetter
+        {
+            get => GetTextureOffset("_lowBumpMap");
+            set => SetTextureOffset("_lowBumpMap", value);
+        }
+
+        // Low Bump Tiling, default = 100000
+        [ParserTarget("lowBumpTiling")]
+        public NumericParser<float> LowBumpTilingSetter
+        {
+            get => GetFloat("_lowBumpTiling");
+            set => SetFloat("_lowBumpTiling", value);
+        }
+
+        // Mid Texture, default = "white" { }
+        [ParserTarget("midTex")]
+        public MaterialTextureParser MidTexSetter
+        {
+            get => null;
+            set => SetTexture("_midTex", value);
+        }
+
+        [ParserTarget("midTexScale")]
+        public Vector2Parser MidTexScaleSetter
+        {
+            get => GetTextureScale("_midTex");
+            set => SetTextureScale("_midTex", value);
+        }
+
+        [ParserTarget("midTexOffset")]
+        public Vector2Parser MidTexOffsetSetter
+        {
+            get => GetTextureOffset("_midTex");
+            set => SetTextureOffset("_midTex", value);
+        }
+
+        // Mid Tiling, default = 100000
+        [ParserTarget("midTiling")]
+        public NumericParser<float> MidTilingSetter
+        {
+            get => GetFloat("_midTiling");
+            set => SetFloat("_midTiling", value);
+        }
+
+        // Mid Bump Map, default = "bump" { }
+        [ParserTarget("midBumpMap")]
+        public MaterialTextureParser MidBumpMapSetter
+        {
+            get => null;
+            set => SetTexture("_midBumpMap", value);
+        }
+
+        [ParserTarget("midBumpMapScale")]
+        public Vector2Parser MidBumpMapScaleSetter
+        {
+            get => GetTextureScale("_midBumpMap");
+            set => SetTextureScale("_midBumpMap", value);
+        }
+
+        [ParserTarget("midBumpMapOffset")]
+        public Vector2Parser MidBumpMapOffsetSetter
+        {
+            get => GetTextureOffset("_midBumpMap");
+            set => SetTextureOffset("_midBumpMap", value);
+        }
+
+        // Mid Bump Tiling, default = 100000
+        [ParserTarget("midBumpTiling")]
+        public NumericParser<float> MidBumpTilingSetter
+        {
+            get => GetFloat("_midBumpTiling");
+            set => SetFloat("_midBumpTiling", value);
+        }
+
+        // High Texture, default = "white" { }
+        [ParserTarget("highTex")]
+        public MaterialTextureParser HighTexSetter
+        {
+            get => null;
+            set => SetTexture("_highTex", value);
+        }
+
+        [ParserTarget("highTexScale")]
+        public Vector2Parser HighTexScaleSetter
+        {
+            get => GetTextureScale("_highTex");
+            set => SetTextureScale("_highTex", value);
+        }
+
+        [ParserTarget("highTexOffset")]
+        public Vector2Parser HighTexOffsetSetter
+        {
+            get => GetTextureOffset("_highTex");
+            set => SetTextureOffset("_highTex", value);
+        }
+
+        // High Tiling, default = 100000
+        [ParserTarget("highTiling")]
+        public NumericParser<float> HighTilingSetter
+        {
+            get => GetFloat("_highTiling");
+            set => SetFloat("_highTiling", value);
+        }
+
+        // High Bump Map, default = "bump" { }
+        [ParserTarget("highBumpMap")]
+        public MaterialTextureParser HighBumpMapSetter
+        {
+            get => null;
+            set => SetTexture("_highBumpMap", value);
+        }
+
+        [ParserTarget("highBumpMapScale")]
+        public Vector2Parser HighBumpMapScaleSetter
+        {
+            get => GetTextureScale("_highBumpMap");
+            set => SetTextureScale("_highBumpMap", value);
+        }
+
+        [ParserTarget("highBumpMapOffset")]
+        public Vector2Parser HighBumpMapOffsetSetter
+        {
+            get => GetTextureOffset("_highBumpMap");
+            set => SetTextureOffset("_highBumpMap", value);
+        }
+
+        // High Bump Tiling, default = 100000
+        [ParserTarget("highBumpTiling")]
+        public NumericParser<float> HighBumpTilingSetter
+        {
+            get => GetFloat("_highBumpTiling");
+            set => SetFloat("_highBumpTiling", value);
+        }
+
+        // Low Transition Start, default = 0
+        [ParserTarget("lowStart")]
+        public NumericParser<float> LowStartSetter
+        {
+            get => GetFloat("_lowStart");
+            set => SetFloat("_lowStart", value);
+        }
+
+        // Low Transition End, default = 0.3
+        [ParserTarget("lowEnd")]
+        public NumericParser<float> LowEndSetter
+        {
+            get => GetFloat("_lowEnd");
+            set => SetFloat("_lowEnd", value);
+        }
+
+        // High Transition Start, default = 0.8
+        [ParserTarget("highStart")]
+        public NumericParser<float> HighStartSetter
+        {
+            get => GetFloat("_highStart");
+            set => SetFloat("_highStart", value);
+        }
+
+        // High Transition End, default = 1
+        [ParserTarget("highEnd")]
+        public NumericParser<float> HighEndSetter
+        {
+            get => GetFloat("_highEnd");
+            set => SetFloat("_highEnd", value);
+        }
+
+        // AP Global Density, default = 1
+        [ParserTarget("globalDensity")]
+        public NumericParser<float> GlobalDensitySetter
+        {
+            get => GetFloat("_globalDensity");
+            set => SetFloat("_globalDensity", value);
+        }
+
+        // FogColorRamp, default = "white" { }
+        [ParserTarget("fogColorRamp")]
+        public MaterialTextureParser FogColorRampSetter
+        {
+            get => null;
+            set => SetTexture("_fogColorRamp", value);
+        }
+
+        [ParserTarget("fogColorRampScale")]
+        public Vector2Parser FogColorRampScaleSetter
+        {
+            get => GetTextureScale("_fogColorRamp");
+            set => SetTextureScale("_fogColorRamp", value);
+        }
+
+        [ParserTarget("fogColorRampOffset")]
+        public Vector2Parser FogColorRampOffsetSetter
+        {
+            get => GetTextureOffset("_fogColorRamp");
+            set => SetTextureOffset("_fogColorRamp", value);
+        }
+
+        // PlanetOpacity, default = 1
+        [ParserTarget("planetOpacity")]
+        public NumericParser<float> PlanetOpacitySetter
+        {
+            get => GetFloat("_PlanetOpacity");
+            set => SetFloat("_PlanetOpacity", value);
+        }
+
+        // Ocean Fog Dist, default = 1000
+        [ParserTarget("oceanFogDistance")]
+        public NumericParser<float> OceanFogDistanceSetter
+        {
+            get => GetFloat("_oceanFogDistance");
+            set => SetFloat("_oceanFogDistance", value);
+        }
+
+        public override ShaderParser ShaderParser { get; set; } = Shader;
+
+        // Constructors
+        public PQSMainTriplanarZoomRotationLoader() { }
+
+        public PQSMainTriplanarZoomRotationLoader(Material material) => Value = new(material);
+    }
+}
