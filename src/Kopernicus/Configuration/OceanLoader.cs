deaf2ff9f377059c7c7e4249019c8a33de518358
Raymond Beehler <21GunSoftware@comcast.net>
2026-05-18 18:47:55 -0700

    Add ocean fix to both constructors.

--- src/Kopernicus/Configuration/OceanLoader.cs
+++ src/Kopernicus/Configuration/OceanLoader.cs
@@ -321,6 +321,10 @@ namespace Kopernicus.Configuration
 
                     Utility.CopyObjectFields(oceans[i], Value);
 
+
+                    // Make sure to set the ocean's layer appropriately
+                    controllerRoot.layer = oceans[i].gameObject.layer;
+
                     // Create the material (always the same shader)
                     BasicSurfaceMaterial = new PQSOceanSurfaceQuad(oceans[i].surfaceMaterial);
                     Value.fallbackMaterial = new PQSOceanSurfaceQuadFallback(oceans[i].fallbackMaterial);
