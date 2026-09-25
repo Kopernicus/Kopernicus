c822f82ad7ef8f12e86c47fa185a36a140dba118
Raymond Beehler <21GunSoftware@comcast.net>
2026-09-25 10:43:57 -0700

    Fix CI build.

--- src/Kopernicus.BurstPQS.KSCScatterMask/Properties/AssemblyInfo.cs
+++ src/Kopernicus.BurstPQS.KSCScatterMask/Properties/AssemblyInfo.cs
@@ -1,14 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("KSCScatterMask Kopernicus BurstPQS Module")]
-[assembly: AssemblyDescription("Gets rid of KSC Scatters.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Linx & R-T-B")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (C) Kopernicus Project 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: KSPAssembly("Kopernicus.KSCScatterMask.BurstPQS", 1, 0)]
 [assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
 [assembly: KSPAssemblyDependency("BurstPQS", 0, 1, 16)]
-[assembly: AssemblyVersion("1.0.0")]
